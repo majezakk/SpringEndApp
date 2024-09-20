2a83f4939d2364e2942839d19eb3b026e90cbccb
zakk_pc <zakhar.dm.ivanov@gmail.com>
2024-09-20 23:24:57 +0300

    Initial commit

---
+++ src/main/java/com/example/SpringEndApp/SpringEndAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringEndApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEndAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEndAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringEndApp/SpringEndAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringEndApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEndAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
